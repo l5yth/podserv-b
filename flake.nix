7ad935e55021ce9bcdfeeef5ea8126c6b2b52f01
l5y <220195275+l5yth@users.noreply.github.com>
2026-03-05 20:14:20 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+# Copyright (c) 2026 l5yth
+# SPDX-License-Identifier: Apache-2.0
+{
+  description = "a minimalist podcast server (type b) for serving media files on the web";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, crane }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+      in {
+        packages.default = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+        };
+      });
+}
